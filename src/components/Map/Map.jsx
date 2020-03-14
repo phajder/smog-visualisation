1a7065189d1941e112736d8b1d3d23a67d0f51c6
Piotr Hajder <phajder@agh.edu.pl>
2020-03-14 14:03:53 +0100

    Simple random GeoJSON visualisation

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
-import { Map as LeafletMap, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map as LeafletMap, GeoJSON, Marker, Popup, TileLayer } from 'react-leaflet';
+import * as turf from '@turf/turf';
+
+function gen() {
+    var extent = [0, 30, 20, 50];
+    var cellWidth = 100;
+    var pointGrid = turf.pointGrid(extent, cellWidth, {units: 'miles'});
+
+    for (var i = 0; i < pointGrid.features.length; i++) {
+    pointGrid.features[i].properties.temperature = Math.random() * 10;
+    }
+    var breaks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    var lines = turf.isolines(pointGrid, breaks, {zProperty: 'temperature'});
+
+    return lines;
+}
 
 export default function Map() {
     const position = [51.984880, 19.368896];
@@ -14,6 +30,9 @@ export default function Map() {
                 A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
             </Marker>
+            <GeoJSON data={gen()} onEachFeature={(feature, layer) => {
+                layer.bindPopup('Hello, I have ' + feature.properties.temperature + ' degrees!');
+            }}/>
         </LeafletMap>
     );
 };
